ef540c18ebfaa31aa5100acb2b2f5933147e2b4d
longcrawfish <266995317+longcrawfish@users.noreply.github.com>
2026-05-15 07:28:12 +0000

    Fix production site URL

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 import { SiteConfig } from "@/types/siteConfig";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://nextjsstarter.io";
+export const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://freeharmonium.com";
 
 export const SOURCE_CODE_URL = "https://github.com/weijunext/nextjs-starter";
 export const PRO_VERSION = "https://nexty.dev";
